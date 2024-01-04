7fe0630ef043deca6f8b21e5320f8c4c00df477c
qyn <qyning710@zju.edu.com>
2024-01-04 03:00:28 +0000

    add testpmd auto run

---
+++ lab_new/lab_afterin_simplex/rule_gen.py
@@ -0,0 +1,14 @@
+import socket
+
+def main():
+    rule_num = 200
+    ip_prefix = 10 << 24
+    command_template = "flow create 0 group 0 transfer pattern eth / ipv4 src is 10.0.0.0 / tcp / end actions port_id id 1 / count / end"
+
+    with open("./rule_testpmd.txt", "w") as file:
+        for i in range(rule_num):
+            src_ip = socket.inet_ntoa((ip_prefix + i).to_bytes(4, byteorder='big'))
+            file.write("flow create 0 group 0 transfer pattern eth / ipv4 src is " + src_ip + " / tcp / end actions port_id id 1 / count / end" + "\n") 
+
+if __name__ == "__main__":
+    main()
