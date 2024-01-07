8ef46da4fa9b06a8801cfc4d18ec4ee757146eda
qyn <qyning710@zju.edu.cn>
2024-01-07 14:28:50 +0800

    update

--- lab_new/lab_afterin_simplex/script/0_run_sta_ipu.sh
+++ lab_new/lab_afterin_simplex/script/0_run_sta_ipu.sh
@@ -10,7 +10,7 @@ line="186-171"
 send_host="186_ali"
 send_ip="22.22.22.186"
 # recv_host="ipuserver"
-send_host="171_ali"
+recv_host="171_ali"
 recv_ip="22.22.22.171"
 user="root"
 run_path="/root/qyn/FastNIC/lab_new/${lab}"
@@ -76,7 +76,7 @@ do
     ssh root@22.22.22.173 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@100.0.0.100 ssh root@192.168.0.2 ${ipu_run_path}/script/ipu_run.sh
 
     #do not start send app until install finish 
-    echo waiting rule instal finish
+    echo waiting rule install finish
     flow_end=$((flow_num-1))
     while true; do
         output=$(ssh root@22.22.22.173 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@100.0.0.100 ssh root@192.168.0.2 "tmux capture-pane -p -t ${ipu_tmux}" | sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' | tail -n 3)
@@ -110,11 +110,11 @@ do
     # echo -e "off_thre,zipf_para\r\n${off_thre},${zipf_para}" > ${run_path}/lab_results/${file}/send_$times/para.csv
     
     tmux send-keys -t ${rcv_tmux} 'quit' C-m
-    tmux capture-pane -pS - -t ${rcv_tmux} >> ${run_path}/lab_results/rcv/rcvtestpmd_${times}.out 2>&1 &
+    tmux capture-pane -pS - -t ${rcv_tmux} >> ${run_path}/lab_results/rcv/rcvtestpmd_${times}.out 2>&1
     tmux kill-session -t ${rcv_tmux}
 
     ssh root@22.22.22.173 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@100.0.0.100 ssh root@192.168.0.2 tmux send-keys -t ${ipu_tmux} "quit" Enter
-    ssh root@22.22.22.173 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@100.0.0.100 ssh root@192.168.0.2 tmux capture-pane -pS - -t ${ipu_tmux} >> ${run_path}/lab_results/ipu_log/ipu_${times}.out 2>&1 &
+    ssh root@22.22.22.173 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@100.0.0.100 ssh root@192.168.0.2 tmux capture-pane -pS - -t ${ipu_tmux} >> ${run_path}/lab_results/ipu_log/ipu_${times}.out 2>&1
     ssh root@22.22.22.173 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@100.0.0.100 ssh root@192.168.0.2 tmux kill-session -t ${ipu_tmux}
     ((times++))
 done
