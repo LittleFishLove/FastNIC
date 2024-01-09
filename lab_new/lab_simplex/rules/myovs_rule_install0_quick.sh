b8d9a8f2399d04d19256591beb06746c2808e7c2
qyn <qyning710@zju.edu.com>
2024-01-09 06:56:19 +0000

    update install

--- lab_new/lab_simplex/rules/myovs_rule_install0_quick.sh
+++ lab_new/lab_simplex/rules/myovs_rule_install0_quick.sh
@@ -9,6 +9,6 @@ O_CIRCLE_NUM=10
 I_CIRCLE_NUM=10000
 for((i=0;i<$O_CIRCLE_NUM;i++));
 do
-  sudo /home/ubuntu/software/ovs_all/ovs_install/usr/bin/ovs-ofctl add-flows ${ovsbr} rules/rule_$i.txt
+  sudo /home/ubuntu/software/ovs_all/ovs_install/usr/bin/ovs-ofctl add-flows ${ovsbr} data/rule_$i.txt
   echo "finish add $(($i*$I_CIRCLE_NUM)) - $((($i+1)*$I_CIRCLE_NUM-1))"
 done
