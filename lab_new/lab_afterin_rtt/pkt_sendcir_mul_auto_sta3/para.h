eb4b07c1abb2f2e6f5d32ec2f72d4aa2cd01fabc
qyn <qyning710@zju.edu.cn>
2024-01-11 11:24:57 +0800

    add pkt_sendcir_mul_auto_sta3

---
+++ lab_new/lab_afterin_rtt/pkt_sendcir_mul_auto_sta3/para.h
@@ -0,0 +1,17 @@
+// #define SRC_IP_NUM 1000
+// #define DST_IP_NUM 1429
+// #define FLOW_NUM 75000
+
+#define FLOW_NUM 75000
+#define FLOW_SIZE 1000
+#define PKT_LEN 64
+#define MAX_RECORD_COUNT 10
+#define PROGRAM "pkt_sendcir_mul_auto_sta3"
+
+// #define PKTS_NUM (FLOW_SIZE*FLOW_NUM)
+#define PKTS_NUM ((2e7) * MAX_RECORD_COUNT)
+
+#define THROUGHPUT_FILE TPUT_PFX "/lab_results/" PROGRAM "/throughput.csv"
+#define THROUGHPUT_TIME_FILE TPUT_PFX "/lab_results/" PROGRAM "/throughput_time.csv"
+
+#define RTT_FILE TPUT_PFX "/lab_results/" PROGRAM "/rtt%d.csv"
