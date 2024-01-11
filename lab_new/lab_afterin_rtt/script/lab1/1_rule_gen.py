2a27e6b39861b65a4da91ec1838ddcca3619dc6c
qyn <qyning710@zju.edu.cn>
2024-01-11 11:15:21 +0800

    update

--- lab_new/lab_afterin_rtt/script/lab1/1_rule_gen.py
+++ lab_new/lab_afterin_rtt/script/lab1/1_rule_gen.py
@@ -7,7 +7,7 @@ def main():
     ip_prefix = 10 << 24
 
     with open("./rule_testpmd.txt", "w") as file:
-        file.write("flow create 0 group 0 ingress pattern eth / end actions jump group 1 / end")
+        file.write("flow create 0 group 0 ingress pattern eth / end actions jump group 1 / end" + "\n")
         for i in range(rule_num):
             src_ip = socket.inet_ntoa((ip_prefix + i).to_bytes(4, byteorder='big'))
             file.write("flow create 0 group 1 ingress pattern eth / ipv4 src is " + src_ip + " / tcp / end actions set_ipv4_dst ipv4_addr 2.2.2.2 / queue index 5 / count / end" + "\n") 
